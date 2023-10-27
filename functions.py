217f6512708646fbe85a65c2c6dc1766f4e4cc77
darshan <darshannaik138@gmail.com>
2023-10-27 18:45:35 +0530

    Initial Commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+FILEPATH = 'todo output.txt'
+
+
+def get_todos(filepath=FILEPATH):
+    with open(filepath, 'r') as file:
+        files = file.readlines()
+    return files
+
+
+def write_todos(todos_arg, filepath=FILEPATH):
+    with open(filepath, 'w') as files:
+        files.writelines(todos_arg)
